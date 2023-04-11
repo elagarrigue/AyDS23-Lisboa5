521839285cd0f8a2bf792d2dfe795b6958932bbe
Matias Pippig <matiaspippig@gmail.com>
2023-04-11 16:45:18 -0300

    Se usan los tipos de dato de Java.time para resolver las fechas en lugar de resolver a traves de un arreglo. Corregido un error de nombre en el metodo formatMonth. Se muestra correctamente el mes y el año cuando la precision es month

--- app/src/main/java/ayds/lisboa/songinfo/home/view/ReleaseDateCreator.kt
+++ app/src/main/java/ayds/lisboa/songinfo/home/view/ReleaseDateCreator.kt
@@ -1,49 +1,41 @@
 package ayds.lisboa.songinfo.home.view
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import java.time.Year
+import java.time.YearMonth
+import java.util.*
+
 interface ReleaseDateCreator{
     fun createDate(precision:String, date:String):String
 }
 internal class ReleaseDateCreatorIml: ReleaseDateCreator {
 
-    private var dateArray = emptyArray<String>()
-
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun createDate(precision: String, date: String): String {
-        dateArray = date.split("-").toTypedArray()
+
         val datePrecision = when (precision) {
-            "day" -> formatDay()
-            "month" -> formatMouth()
-            "year" -> formatYear()
+            "day" -> formatDay(LocalDate.parse(date))
+            "month" -> formatMonth(YearMonth.parse(date))
+            "year" -> formatYear(Year.parse(date))
             else -> "ERROR when(releaseDatePrecision)"
         }
         return datePrecision
     }
 
-    private fun formatYear() = "${dateArray[0]}, (${leapYear(dateArray[0].toInt())})"
-    private fun formatMouth() = "${getMonth(dateArray[1].toInt())}, ${dateArray[0]}"
-    private fun formatDay() = "${dateArray[0]}/${dateArray[1]}/${dateArray[2]}"
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun formatYear(year: Year) = "${year}, (${leapYear(year)})"
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun formatMonth(month: YearMonth) =
+        "${month.month.toString().substring(0, 1) + 
+                month.month.toString().substring(1).lowercase()}, ${month.year}"
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun formatDay(date: LocalDate) = "${date.dayOfMonth}/${date.monthValue}/${date.year}"
 
-    private fun getMonth(month: Int) = when (month) {
-        1 -> "January"
-        2 -> "February"
-        3 -> "March"
-        4 -> "April"
-        5 -> "May"
-        6 -> "June"
-        7 -> "July"
-        8 -> "August"
-        9 -> "September"
-        10 -> "October"
-        11 -> "November"
-        12 -> "December"
-        else -> "ERROR getMonth($month)"
-    }
-
-    private fun isLeapYear(year: Int): Boolean {
-        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
-    }
-
-    private fun leapYear(year: Int) : String {
-        return if (isLeapYear(year))
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun leapYear(year: Year) : String {
+        return if (year.isLeap)
             "leap year"
         else
             "not leap year"
