083eeabd530268fdde3b34f8dc389b29a5726618
AxelFontana <axelfontana01@gmail.com>
2023-04-15 16:09:04 -0300

    Clase DateWrapper eliminada, se usan split en dateFormatFactory y los factorys concretos reciben la fecha en un array de string
    Co-authored-by: MartinVerniere <martincho7verniere@yahoo.com>
    Co-authored-by: MatuP1 <matiaspippig@gmail.com>
    Co-authored-by: KatherinaVilca <katherinavilca@gmail.com>

--- app/src/main/java/ayds/lisboa/songinfo/home/view/DateFormatFactory.kt
+++ app/src/main/java/ayds/lisboa/songinfo/home/view/DateFormatFactory.kt
@@ -1,33 +1,47 @@
-import ayds.lisboa.songinfo.home.view.DateWrapper
-
 object DateFormatFactory {
     private const val DAY ="day"
     private const val MONTH ="month"
     private const val YEAR ="year"
-    fun get(precision: String, date: String) = when (precision) {
-        DAY -> DayFactory(date)
-        MONTH -> MonthFactory(date)
-        YEAR -> YearFactory(date)
-        else -> DefaultFactory(date)
+    var dateArray = emptyArray<String>()
+    fun get(precision: String, date: String):DateCreator{
+        dateArray = date.split("-").toTypedArray()
+        return when (precision) {
+            DAY -> DayFactory(dateArray)
+            MONTH -> MonthFactory(dateArray)
+            YEAR -> YearFactory(dateArray)
+            else -> DefaultFactory(dateArray)
+        }
     }
 }
 
-sealed class DateCreator(val date: String) {
+sealed class DateCreator(val dateArray: Array<String>) {
     abstract fun createDate(): String
 }
 
-private class DayFactory(date: String) : DateCreator(date) {
-    override fun createDate() = DateWrapper(date).formatDay()
+private class DayFactory(dateArray: Array<String>) : DateCreator(dateArray) {
+    override fun createDate() = "${dateArray[2]}/${dateArray[1]}/${dateArray[0]}"
 }
 
-private class MonthFactory(date: String): DateCreator(date) {
-    override fun createDate() = DateWrapper(date).formatMonth()
+private class MonthFactory(dateArray: Array<String>): DateCreator(dateArray) {
+    private val monthArray = arrayOf( "January","February","March","April","May","June","July","August","September","October","November","December" )
+    override fun createDate() = "${getMonth(dateArray[1].toInt())}, ${dateArray[0]}"
+
+    private fun getMonth(month: Int) = if (month in 1..12)  monthArray[month-1] else "ERROR getMonth(month)"
 }
 
-private class YearFactory(date: String): DateCreator(date) {
-    override fun createDate()= DateWrapper(date).formatYear()
+private class YearFactory(dateArray: Array<String>): DateCreator(dateArray) {
+    override fun createDate()= "${dateArray[0]}, (${leapYear(dateArray[0].toInt())})"
+
+    private fun leapYear(year: Int) = if (isLeapYear(year)) "leap year" else "not leap year"
+    private fun isLeapYear(year: Int) = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 }
 
-private class DefaultFactory(date: String): DateCreator(date) {
-    override fun createDate()= date
+private class DefaultFactory(dateArray: Array<String>): DateCreator(dateArray) {
+    override fun createDate()= dateArray.joinToString("-")
 }
+
+
+
+
+
+
