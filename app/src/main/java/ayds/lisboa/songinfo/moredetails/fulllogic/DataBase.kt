9399fce2a0a380c5077d26722a7db720e683b4bd
Matias Pippig <matiaspippig@gmail.com>
2023-04-19 10:55:09 -0300

    funciones estaticas transformadas a normales saveArtist y getInfo

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
@@ -16,58 +16,54 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 
-    companion object {
-        @JvmStatic
-        fun saveArtist(dbHelper: DataBase, artist: String?, info: String?) {
-            // Gets the data repository in write mode
-            val db = dbHelper.writableDatabase
+    fun saveArtist(artist: String?, info: String?) {
+    // Gets the data repository in write mode
+    val db = this.writableDatabase
 
 // Create a new map of values, where column names are the keys
-            val values = ContentValues()
-            values.put("artist", artist)
-            values.put("info", info)
-            values.put("source", 1)
+        val values = ContentValues()
+        values.put("artist", artist)
+        values.put("info", info)
+        values.put("source", 1)
 
 // Insert the new row, returning the primary key value of the new row
-            val newRowId = db.insert("artists", null, values)
-        }
+        val newRowId = db.insert("artists", null, values)
+    }
 
-        @JvmStatic
-        fun getInfo(dbHelper: DataBase, artist: String): String? {
-            val db = dbHelper.readableDatabase
+    fun getInfo(artist: String): String? {
+        val db = this.readableDatabase
 
 // Define a projection that specifies which columns from the database
 // you will actually use after this query.
-            val projection = arrayOf(
-                "id",
-                "artist",
-                "info"
-            )
+        val projection = arrayOf(
+            "id",
+            "artist",
+            "info"
+        )
 
 // Filter results WHERE "title" = 'My Title'
-            val selection = "artist  = ?"
-            val selectionArgs = arrayOf(artist)
+        val selection = "artist  = ?"
+        val selectionArgs = arrayOf(artist)
 
 // How you want the results sorted in the resulting Cursor
-            val sortOrder = "artist DESC"
-            val cursor = db.query(
-                "artists",  // The table to query
-                projection,  // The array of columns to return (pass null to get all)
-                selection,  // The columns for the WHERE clause
-                selectionArgs,  // The values for the WHERE clause
-                null,  // don't group the rows
-                null,  // don't filter by row groups
-                sortOrder // The sort order
+        val sortOrder = "artist DESC"
+        val cursor = db.query(
+            "artists",  // The table to query
+            projection,  // The array of columns to return (pass null to get all)
+            selection,  // The columns for the WHERE clause
+            selectionArgs,  // The values for the WHERE clause
+            null,  // don't group the rows
+            null,  // don't filter by row groups
+            sortOrder // The sort order
+        )
+        val items: MutableList<String> = ArrayList()
+        while (cursor.moveToNext()) {
+            val info = cursor.getString(
+                cursor.getColumnIndexOrThrow("info")
             )
-            val items: MutableList<String> = ArrayList()
-            while (cursor.moveToNext()) {
-                val info = cursor.getString(
-                    cursor.getColumnIndexOrThrow("info")
-                )
-                items.add(info)
-            }
-            cursor.close()
-            return if (items.isEmpty()) null else items[0]
+            items.add(info)
         }
+        cursor.close()
+        return if (items.isEmpty()) null else items[0]
     }
 }
