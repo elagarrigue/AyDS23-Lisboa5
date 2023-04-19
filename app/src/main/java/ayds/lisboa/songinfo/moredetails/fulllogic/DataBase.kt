5cf3af17a8142f588ac7a73b68e4b6b685e59072
Matias Pippig <matiaspippig@gmail.com>
2023-04-19 10:51:05 -0300

    funcion testDb muerta eliminada y sus side efects

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
@@ -5,9 +5,6 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
 
 class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", null, 1) {
     override fun onCreate(db: SQLiteDatabase) {
@@ -20,40 +17,6 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 
     companion object {
-        fun testDB() {
-            var connection: Connection? = null
-            try {
-                // create a database connection
-                connection = DriverManager.getConnection("jdbc:sqlite:./dictionary.db")
-                val statement = connection.createStatement()
-                statement.queryTimeout = 30 // set timeout to 30 sec.
-
-                //statement.executeUpdate("drop table if exists person");
-                //statement.executeUpdate("create table person (id integer, name string)");
-                //statement.executeUpdate("insert into person values(1, 'leo')");
-                //statement.executeUpdate("insert into person values(2, 'yui')");
-                val rs = statement.executeQuery("select * from artists")
-                while (rs.next()) {
-                    // read the result set
-                    println("id = " + rs.getInt("id"))
-                    println("artist = " + rs.getString("artist"))
-                    println("info = " + rs.getString("info"))
-                    println("source = " + rs.getString("source"))
-                }
-            } catch (e: SQLException) {
-                // if the error message is "out of memory",
-                // it probably means no database file is found
-                System.err.println(e.message)
-            } finally {
-                try {
-                    connection?.close()
-                } catch (e: SQLException) {
-                    // connection close failed.
-                    System.err.println(e)
-                }
-            }
-        }
-
         @JvmStatic
         fun saveArtist(dbHelper: DataBase, artist: String?, info: String?) {
             // Gets the data repository in write mode
