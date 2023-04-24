1944b1f7b5770e453d3fd69dd7af873274ef6cc6
Katherina <katherinavilca@gmail.com>
2023-04-23 23:56:33 -0300

    Nombres mas significativos a variables. Variables intermedias para hacer mas legible el codigo y se desdoblo en dos a la funcion getInfo para lograr claridad en el codigo.

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
@@ -2,6 +2,7 @@ package ayds.lisboa.songinfo.moredetails.fulllogic
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
@@ -17,53 +18,45 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 
     fun saveArtist(artist: String?, info: String?) {
-    // Gets the data repository in write mode
-    val db = this.writableDatabase
-
-// Create a new map of values, where column names are the keys
+        val database = this.writableDatabase
         val values = ContentValues()
         values.put("artist", artist)
         values.put("info", info)
         values.put("source", 1)
 
-// Insert the new row, returning the primary key value of the new row
-        db.insert("artists", null, values)
+        database.insert("artists", null, values)
     }
 
     fun getInfo(artist: String): String? {
-        val db = this.readableDatabase
-
-// Define a projection that specifies which columns from the database
-// you will actually use after this query.
-        val projection = arrayOf(
+        val database = this.readableDatabase
+        val table = "artists"
+        val columnsToSelect= arrayOf(
             "id",
             "artist",
             "info"
         )
-
-// Filter results WHERE "title" = 'My Title'
-        val selection = "artist  = ?"
         val selectionArgs = arrayOf(artist)
-
-// How you want the results sorted in the resulting Cursor
         val sortOrder = "artist DESC"
-        val cursor = db.query(
-            "artists",  // The table to query
-            projection,  // The array of columns to return (pass null to get all)
-            selection,  // The columns for the WHERE clause
+        val cursor = database.query(
+            table,
+            columnsToSelect,
+            artist,
             selectionArgs,  // The values for the WHERE clause
-            null,  // don't group the rows
-            null,  // don't filter by row groups
-            sortOrder // The sort order
+            null,
+            null,
+            sortOrder
         )
-        val items: MutableList<String> = ArrayList()
+        return searchInfoArtist(cursor)
+    }
+
+    private fun searchInfoArtist(cursor: Cursor): String? {
+        val infoArtist: MutableList<String> = ArrayList()
         while (cursor.moveToNext()) {
-            val info = cursor.getString(
-                cursor.getColumnIndexOrThrow("info")
-            )
-            items.add(info)
+            val numberColum = cursor.getColumnIndexOrThrow("info")
+            val info = cursor.getString(numberColum)
+            infoArtist.add(info)
         }
         cursor.close()
-        return if (items.isEmpty()) null else items[0]
+        return if (infoArtist.isEmpty()) null else infoArtist[0]
     }
 }
