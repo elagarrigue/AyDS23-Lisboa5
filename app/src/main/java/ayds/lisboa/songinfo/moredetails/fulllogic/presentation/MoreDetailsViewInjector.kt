2760ed16f90212555c7318784bad78062fff32d6
AxelFontana <axelfontana01@gmail.com>
2023-05-08 18:38:59 -0300

    MoreDetailsInjecto SHELL added

---
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/MoreDetailsViewInjector.kt
@@ -0,0 +1,9 @@
+package ayds.lisboa.songinfo.moredetails.fulllogic.presentation
+
+object MoreDetailsViewInjector {
+    val artistDescriptionHelper: ArtistDescriptionHelper = ArtistDescriptionHelperImpl()
+
+    fun init(moreDetailsView: MoreDetailsView){
+
+    }
+}
