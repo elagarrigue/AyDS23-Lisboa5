3a061b7488c968ab455da2479f62b5ffd62d93c8
AxelFontana <axelfontana01@gmail.com>
2023-05-08 19:58:15 -0300

    MoreDetailsModel incializated, the presenter needs to be initialized

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/MoreDetailsViewInjector.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/MoreDetailsViewInjector.kt
@@ -1,9 +1,12 @@
 package ayds.lisboa.songinfo.moredetails.fulllogic.presentation
 
+import ayds.lisboa.songinfo.moredetails.fulllogic.domain.MoreDetailsModelInjector
+
 object MoreDetailsViewInjector {
     val artistDescriptionHelper: ArtistDescriptionHelper = ArtistDescriptionHelperImpl()
 
     fun init(moreDetailsView: MoreDetailsView){
-
+        MoreDetailsModelInjector.initMoreDetailsModel(moreDetailsView)
+        //Falta inicializar el Presenter
     }
 }
