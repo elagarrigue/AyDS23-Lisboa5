ba2d8f2b5adf9fe884c525f24fe15270bfdfc73e
Martin Verniere <martincho7verniere@yahoo.com>
2023-05-09 18:08:05 -0300

    Added MoreDetails() event in MoreDetailsPresenter.kt
    Co-authored-by: AxelFontana <axelfontana09@gmail.com>

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/MoreDetailsUiEvent.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/MoreDetailsUiEvent.kt
@@ -2,4 +2,6 @@ package ayds.lisboa.songinfo.moredetails.fulllogic.presentation
 
 sealed class MoreDetailsUiEvent {
     object OpenArtistUrl: MoreDetailsUiEvent()
+
+    object MoreDetails: MoreDetailsUiEvent()
 }
