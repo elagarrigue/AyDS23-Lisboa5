c1e162c31659185d7d3711f47f19355ebe9accc3
AxelFontana <axelfontana01@gmail.com>
2023-05-09 10:34:28 -0300

    Now the observer inicialization will have values for different events, MoreDetailsView  setted

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/MoreDetailsPresenter.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/MoreDetailsPresenter.kt
@@ -1,26 +1,39 @@
 package ayds.lisboa.songinfo.moredetails.fulllogic.presentation
 
 
+import ayds.lisboa.songinfo.moredetails.fulllogic.domain.MoreDetailsModel
 import ayds.observer.Observer
 
 
-public interface MoreDetailsPresenter{
-
+interface MoreDetailsPresenter{
+    fun setMoreDetailsView(moreDetailsView: MoreDetailsView)
 }
 
-internal class OtherInfoPresenterImpl: MoreDetailsPresenter{
+internal class OtherInfoPresenterImpl(
+    private val moreDetailsModel: MoreDetailsModel
+): MoreDetailsPresenter{
 
     private lateinit var moreDetailsView: MoreDetailsView
 
+    override fun setMoreDetailsView(moreDetailsView: MoreDetailsView) {
+        this.moreDetailsView = moreDetailsView
+        moreDetailsView.uiEventObservable.subscribe(observer)
+    }
+
+    //Va a tener otro para iniciarlizar el panel, ya que esta va a ser llamado de homeview(para detectar la actividad esa)?
     private val observer: Observer<MoreDetailsUiEvent> =
-        Observer {
-            openUrl()
+        Observer { value ->
+            when(value){
+               MoreDetailsUiEvent.OpenArtistUrl -> openUrl()
+            }
         }
 
     private fun openUrl(){
         val artistUrl = moreDetailsView.uiState.artistURL
         moreDetailsView.openUrl(artistUrl)
     }
+
+
 
 
 
