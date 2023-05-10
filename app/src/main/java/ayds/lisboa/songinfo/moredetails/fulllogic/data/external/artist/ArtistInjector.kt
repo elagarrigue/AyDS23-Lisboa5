78d4182f1b8264a3844e6c08ca3fcf33650025f8
Martin Verniere <martincho7verniere@yahoo.com>
2023-05-10 12:30:35 -0300

    Deleted unnecesary classes
    Co-authored-by: AxelFontana <axelfontana09@gmail.com>

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/data/external/artist/ArtistInjector.kt
+++
@@ -1,8 +0,0 @@
-package ayds.lisboa.songinfo.moredetails.fulllogic.data.external.artist
-
-import ayds.lisboa.songinfo.moredetails.fulllogic.data.external.artist.artists.ArtistExternalInjector
-import ayds.lisboa.songinfo.moredetails.fulllogic.data.external.artist.artists.ArtistExternalService
-
-object ArtistInjector {
-    val artistExternalService : ArtistExternalService = ArtistExternalInjector.artistExternalService
-}
--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/data/local/artist/ArtistLocalStorage.kt
+++
@@ -1,9 +0,0 @@
-package ayds.lisboa.songinfo.moredetails.fulllogic.data.local.artist
-
-import ayds.lisboa.songinfo.moredetails.fulllogic.domain.Artist
-
-interface ArtistLocalStorage {
-    fun getArtist(artistName: String): Artist
-
-    fun saveArtistInfo(artist: Artist?)
-}
