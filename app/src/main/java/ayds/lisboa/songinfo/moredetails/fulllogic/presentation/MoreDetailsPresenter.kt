fa2959a63a34bbe5a2c50a68887c97fbc810505c
Martin Verniere <martincho7verniere@yahoo.com>
2023-05-11 19:06:09 -0300

    Class indentation
    Co-authored-by: AxelFontana <axelfontana09@gmail.com>

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/MoreDetailsPresenter.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/presentation/MoreDetailsPresenter.kt
@@ -5,54 +5,58 @@ import ayds.lisboa.songinfo.moredetails.fulllogic.domain.ArtistRepository
 import ayds.observer.Observable
 import ayds.observer.Subject
 
-interface MoreDetailsPresenter{
+interface MoreDetailsPresenter {
     val artistObservable: Observable<MoreDetailsUiState>
 
     fun setRepository(repository: ArtistRepository)
     fun moreDetails(artistName: String)
 }
 
-internal class MoreDetailsPresenterImpl(private val artistDescriptionHelper: ArtistDescriptionHelper): MoreDetailsPresenter{
+internal class MoreDetailsPresenterImpl(private val artistDescriptionHelper: ArtistDescriptionHelper) :
+    MoreDetailsPresenter {
 
     private lateinit var repository: ArtistRepository
 
     override val artistObservable = Subject<MoreDetailsUiState>()
 
     override fun setRepository(repository: ArtistRepository) {
-        this.repository=repository
+        this.repository = repository
     }
 
-    override fun moreDetails(artistName: String){
+    override fun moreDetails(artistName: String) {
         Thread {
-            val artist=repository.getArtist(artistName)
+            val artist = repository.getArtist(artistName)
             val reformattedText = artistDescriptionHelper.getArtistDescription(artist)
-            val uiState = updateArtistUiState(artist,reformattedText)
+            val uiState = updateArtistUiState(artist, reformattedText)
             uiState.let {
                 artistObservable.notify(it)
             }
         }.start()
     }
 
-    private fun updateArtistUiState(artist: Artist,reformattedText: String): MoreDetailsUiState {
+    private fun updateArtistUiState(artist: Artist, reformattedText: String): MoreDetailsUiState {
         return when (artist) {
-            is Artist.ArtistData -> updateUiState(artist,reformattedText)
+            is Artist.ArtistData -> updateUiState(artist, reformattedText)
             Artist.EmptyArtist -> updateNoResultsUiState()
         }
     }
 
-    private fun updateUiState(artist: Artist.ArtistData,reformattedText: String): MoreDetailsUiState{
+    private fun updateUiState(
+        artist: Artist.ArtistData,
+        reformattedText: String
+    ): MoreDetailsUiState {
         return MoreDetailsUiState(
             artistName = artist.artistName,
-            artistBioContent= reformattedText,
-            artistURL= artist.artistURL,
+            artistBioContent = reformattedText,
+            artistURL = artist.artistURL,
         )
     }
 
-    private fun updateNoResultsUiState(): MoreDetailsUiState{
+    private fun updateNoResultsUiState(): MoreDetailsUiState {
         return MoreDetailsUiState(
             artistName = "",
-            artistBioContent= "No Results",
-            artistURL= "",
+            artistBioContent = "No Results",
+            artistURL = "",
         )
     }
 }
