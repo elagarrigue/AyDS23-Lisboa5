6e206150708e9079d6b4352b5b8b9f26972a010b
Katherina <katherinavilca@gmail.com>
2023-05-15 14:27:54 -0300

    Test: existe artista no nulo en la db y lo marca como local. + no existe artista (es Empty) , lo busca en LastFMPI y guarda la info (falta corregir)

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/data/ArtistRepositoryTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/data/ArtistRepositoryTest.kt
@@ -1,7 +1,15 @@
 package ayds.lisboa.songinfo.moredetails.data
 
-import org.junit.Assert.*
-
+import ayds.lisboa.songinfo.moredetails.data.external.artist.ArtistExternalService
+import ayds.lisboa.songinfo.moredetails.data.local.sqldb.ArtistLocalStorage
+import ayds.lisboa.songinfo.moredetails.domain.entities.Artist
+import ayds.lisboa.songinfo.moredetails.domain.repository.ArtistRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class ArtistRepositoryTest {
@@ -9,4 +17,37 @@ class ArtistRepositoryTest {
     @Test
     fun getArtist() {
     }
+
+    private val artistLocalStorage: ArtistLocalStorage = mockk(relaxUnitFun = true)
+    private val artistExternalService: ArtistExternalService = mockk(relaxUnitFun = true)
+
+    private val artistRepository: ArtistRepository by lazy {
+        ArtistRepositoryImpl(artistLocalStorage, artistExternalService)
+    }
+
+    @Test
+    fun `given existing artist by artistName should return artist and mark it as local`() {
+        val artist = Artist.ArtistData("artistName","artistBioContent","artistURL",false)
+        every{ artistLocalStorage.getArtist("artistName")} returns artist
+
+        val resultado = artistRepository.getArtist("artistName")
+        assertEquals(artist, resultado)
+        assertTrue(artist.isLocallyStored)
+    }
+
+    @Test
+    fun `given non existing song by artistName should get the artist and store it`() {
+        val artist = Artist.ArtistData("artistName","artistBioContent","artistURL",false)
+        every { artistLocalStorage.getArtist("artistName") } returns Artist.EmptyArtist
+        every {artistExternalService.getArtistFromLastFMAPI("artistName") } returns Artist.ArtistData("artistName","artistBioContent","artistURL",false)
+
+
+        val result = artistRepository.getArtist("artistName")
+
+        assertEquals(artist, result)
+        Assert.assertFalse(artist.isLocallyStored)
+        verify { ArtistRepositoryImpl.saveArtistInfo(artist) }
+    }
+
+
 }
