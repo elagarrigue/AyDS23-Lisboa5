977ec570f96d6c0f74361682b4157908ee2f93ff
Matias Pippig <matiaspippig@gmail.com>
2023-05-15 11:45:32 -0300

    Creada la carpeta para almacenar los test y el test ArtistExternalServiceImplTest.kt

---
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/data/external/artist/ArtistExternalServiceImplTest.kt
@@ -0,0 +1,12 @@
+package ayds.lisboa.songinfo.moredetails.data.external.artist
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class ArtistExternalServiceImplTest {
+
+    @Test
+    fun getArtistFromLastFMAPI() {
+    }
+}
