6e8030a26a21182659c13608ddc40bb6c5ad1e2f
Matias Pippig <matiaspippig@gmail.com>
2023-05-15 17:16:56 -0300

    Cascara de los test a realizar en MoreDetailsPresenterTest.kt, no supe como realizar los tests

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/MoreDetailsPresenterTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/MoreDetailsPresenterTest.kt
@@ -1,44 +1,53 @@
 package ayds.lisboa.songinfo.moredetails.presentation
 
+import ayds.lisboa.songinfo.home.controller.HomeControllerImpl
+import ayds.lisboa.songinfo.home.model.HomeModel
+import ayds.lisboa.songinfo.home.model.entities.Song
+import ayds.lisboa.songinfo.home.view.HomeUiEvent
+import ayds.lisboa.songinfo.home.view.HomeUiState
+import ayds.lisboa.songinfo.home.view.HomeView
 import ayds.lisboa.songinfo.moredetails.domain.entities.Artist
 import ayds.lisboa.songinfo.moredetails.injector.MoreDetailsInjector
+import ayds.observer.Subject
 import io.mockk.InternalPlatformDsl.toArray
 import io.mockk.InternalPlatformDsl.toStr
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Assert.*
 import org.junit.Before
 
 import org.junit.Test
+import kotlin.math.log
 
 class MoreDetailsPresenterTest {
+    private val homeModel: HomeModel = mockk(relaxUnitFun = true)
+
     private val artistDescriptionHelper : ArtistDescriptionHelper = mockk(relaxUnitFun = true);
-    private val moreDetailsPresenter : MoreDetailsPresenter by lazy {
-        MoreDetailsPresenterImpl(artistDescriptionHelper)
+
+    private val onActionSubject = Subject<HomeUiEvent>()
+    private val homeView: HomeView = mockk(relaxUnitFun = true) {
+        every { uiEventObservable } returns onActionSubject
     }
-    private val moreDetailsInjector = MoreDetailsInjector
-    private val moreDetailsView : MoreDetailsView = MoreDetailsViewActivity();
+
+    private val homeController by lazy {
+        HomeControllerImpl(homeModel)
+    }
+    private val moreDetailsPresenter : MoreDetailsPresenter = mockk(relaxUnitFun = true)
     @Before
-    fun setup (){
-        moreDetailsInjector.init(moreDetailsView)
+    fun setup() {
+        homeController.setHomeView(homeView)
     }
     @Test
     fun `when the more details of an LastFM artist that is not store locally are updated should update`() {
-        val artist: Artist.ArtistData = Artist.ArtistData(
-            "Bizarrap",
-            "Gonzalo Julián Conde, conocido artísticamente como Bizarrap, es un productor musical y DJ argentino. Se especializa en géneros como el trap, la música electrónica y el rap. Es conocido por sus Bzrp Music Session y sus Bzrp Freestyle Sessions, las que realiza junto a distintos artistas <a href=\"https://www.last.fm/music/Bizarrap\">Read more on Last.fm</a>. User-contributed text is available under the Creative Commons By-SA License; additional terms may apply.",
-            "https://www.last.fm/music/Bizarrap",
-            false
-        )
-        val expected = MoreDetailsUiState(
-            "Bizarrap",
-            "Gonzalo Julián Conde, conocido artísticamente como Bizarrap, es un productor musical y DJ argentino. Se especializa en géneros como el trap, la música electrónica y el rap. Es conocido por sus Bzrp Music Session y sus Bzrp Freestyle Sessions, las que realiza junto a distintos artistas <a href=\"https://www.last.fm/music/Bizarrap\">Read more on Last.fm</a>. User-contributed text is available under the Creative Commons By-SA License; additional terms may apply.",
-            "https://www.last.fm/music/Bizarrap",
 
-        )
+    }
+    @Test
+    fun `when the more details of an locally store artist are updated should update`() {
 
-        moreDetailsPresenter.moreDetails(artist.artistName)
+    }
+    @Test
+    fun `when the more details of an empty artist are updated should stay update the bio content with no result`() {
 
-        val result = MoreDetailsUiState()
-        assertEquals(expected, result)
     }
 }
