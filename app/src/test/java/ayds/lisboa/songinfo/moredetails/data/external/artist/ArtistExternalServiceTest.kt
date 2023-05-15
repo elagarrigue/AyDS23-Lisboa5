b51ba08e32e3d62efd460e88d074afd6fcfaa2a7
Matias Pippig <matiaspippig@gmail.com>
2023-05-15 15:08:49 -0300

    Test para recuperar los detalles de un artista desde internet con errores

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/data/external/artist/ArtistExternalServiceTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/data/external/artist/ArtistExternalServiceTest.kt
@@ -1,10 +1,26 @@
 package ayds.lisboa.songinfo.moredetails.data.external.artist
 
+import ayds.lisboa.songinfo.home.model.repository.SongRepository
+import ayds.lisboa.songinfo.home.model.repository.SongRepositoryImpl
+import ayds.lisboa.songinfo.moredetails.data.local.sqldb.ArtistLocalStorage
+import ayds.lisboa.songinfo.moredetails.domain.entities.Artist
+import io.mockk.every
+import io.mockk.mockk
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 
 class ArtistExternalServiceTest {
+    private val lastFMAPI:LastFMAPI = mockk(relaxUnitFun = true);
+    private val lastFMToArtistResolver : LastFMToArtistResolver = mockk(relaxUnitFun = true);
+    private val artistExternalService : ArtistExternalService by lazy {
+        ArtistExternalServiceImpl(lastFMAPI,lastFMToArtistResolver)
+    }
 
     @Test
-    fun getArtistFromLastFMAPI() {
+    fun  `given existing artist name should return artist`(){
+        val artist:Artist.ArtistData = mockk();
+        every { artistExternalService.getArtistFromLastFMAPI("artistName")} returns artist
+        val result = artistExternalService.getArtistFromLastFMAPI("artistName");
+        assertEquals(artist,result)
     }
 }
