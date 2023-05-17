c9a0597097ef4895a891f1ad673720ed24600dce
Matias Pippig <matiaspippig@gmail.com>
2023-05-17 12:11:12 -0300

    Test para ArtistDescriptionHelperTest.kt simplificado

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/ArtistDescriptionHelperTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/presentation/ArtistDescriptionHelperTest.kt
@@ -1,6 +1,5 @@
 package ayds.lisboa.songinfo.moredetails.presentation
 
-import ayds.lisboa.songinfo.home.model.entities.Song
 import ayds.lisboa.songinfo.moredetails.domain.entities.Artist
 import io.mockk.mockk
 import org.junit.Assert.*
@@ -13,24 +12,24 @@ class ArtistDescriptionHelperTest {
     fun `given a non local artist it should return the description`() {
         val artist: Artist = Artist.ArtistData(
         "Bizarrap",
-        "Gonzalo Julián Conde, conocido artísticamente como Bizarrap, es un productor musical y DJ argentino. Se especializa en géneros como el trap, la música electrónica y el rap. Es conocido por sus Bzrp Music Session y sus Bzrp Freestyle Sessions, las que realiza junto a distintos artistas <a href=\"https://www.last.fm/music/Bizarrap\">Read more on Last.fm</a>. User-contributed text is available under the Creative Commons By-SA License; additional terms may apply.",
+        "Vida y obra de bizarrap",
         "https://www.last.fm/music/Bizarrap",
         false
         )
-        val result = artistDescriptionHelper.getArtistDescription(artist);
-        val expected = "<html><div width=400><font face=\"arial\">Gonzalo Julián Conde, conocido artísticamente como <b>BIZARRAP</b>, es un productor musical y DJ argentino. Se especializa en géneros como el trap, la música electrónica y el rap. Es conocido por sus Bzrp Music Session y sus Bzrp Freestyle Sessions, las que realiza junto a distintos artistas <a href=\"https://www.last.fm/music/<b>BIZARRAP</b>\">Read more on Last.fm</a>. User-contributed text is available under the Creative Commons By-SA License; additional terms may apply.</font></div></html>"
+        val result = artistDescriptionHelper.getArtistDescription(artist)
+        val expected = "<html><div width=400><font face=\"arial\">Vida y obra de <b>BIZARRAP</b></font></div></html>"
         assertEquals(expected, result)
     }
     @Test
     fun `given a local artist it should return the description with the prefix`() {
         val artist: Artist = Artist.ArtistData(
             "Bizarrap",
-            "Gonzalo Julián Conde, conocido artísticamente como Bizarrap, es un productor musical y DJ argentino. Se especializa en géneros como el trap, la música electrónica y el rap. Es conocido por sus Bzrp Music Session y sus Bzrp Freestyle Sessions, las que realiza junto a distintos artistas <a href=\"https://www.last.fm/music/Bizarrap\">Read more on Last.fm</a>. User-contributed text is available under the Creative Commons By-SA License; additional terms may apply.",
+            "Vida y obra de bizarrap",
             "https://www.last.fm/music/Bizarrap",
             true
         )
-        val result = artistDescriptionHelper.getArtistDescription(artist);
-        val expected = "<html><div width=400><font face=\"arial\">[*]Gonzalo Julián Conde, conocido artísticamente como <b>BIZARRAP</b>, es un productor musical y DJ argentino. Se especializa en géneros como el trap, la música electrónica y el rap. Es conocido por sus Bzrp Music Session y sus Bzrp Freestyle Sessions, las que realiza junto a distintos artistas <a href=\"https://www.last.fm/music/<b>BIZARRAP</b>\">Read more on Last.fm</a>. User-contributed text is available under the Creative Commons By-SA License; additional terms may apply.</font></div></html>"
+        val result = artistDescriptionHelper.getArtistDescription(artist)
+        val expected = "<html><div width=400><font face=\"arial\">[*]Vida y obra de <b>BIZARRAP</b></font></div></html>"
         assertEquals(expected, result)
     }
     @Test
