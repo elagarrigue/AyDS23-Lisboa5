d15cdf9e477e8d44ac61b83cd27bdf1390bc774b
Matias Pippig <matiaspippig@gmail.com>
2023-05-29 16:48:18 -0300

    Agregue la constante con la Url del logo de last fm a la entidad Artist.kt, corregidos errores en ProxyLastFM.kt

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/cards/proxy/ProxyLastFM.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/cards/proxy/ProxyLastFM.kt
@@ -1,6 +1,7 @@
 package ayds.lisboa.songinfo.moredetails.cards.proxy
 
 import ayds.lisboa.songinfo.moredetails.cards.Card
+import lisboa5lastfm.LASTFM_LOGO_URL
 import lisboa5lastfm.artist.ArtistExternalService
 
 class ProxyLastFM(
@@ -8,14 +9,15 @@ class ProxyLastFM(
 ) {
 
     fun getCard(artistName: String): Card {
-        var artistLastFMInfo = lastFMAPI.getArtistFromLastFMAPI(artistName)
+        val artistLastFMInfo = lastFMAPI.getArtistFromLastFMAPI(artistName)
         return when {
             (artistLastFMInfo == null) -> Card.EmptyCard
             else -> Card.CardData(
                 "LastFM",
                 artistLastFMInfo.artistBioContent,
                 artistLastFMInfo.artistURL,
-                artistLastFMInfo.ImageURL, //Como obtener ImageURL???
+                LASTFM_LOGO_URL, //Como obtener ImageURL???
             )
         }
+    }
 }
