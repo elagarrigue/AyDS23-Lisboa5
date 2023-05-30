d65a084665900fa68e85b50752232bf49b26d05a
MartinVerniere <martincho7verniere@yahoo.com>
2023-05-30 18:26:05 -0300

    Update MoreDetailsView.kt
    Co-Authored-By: AxelK12 <51532945+AxelFontana@users.noreply.github.com>

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/presentation/MoreDetailsView.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/presentation/MoreDetailsView.kt
@@ -16,12 +16,22 @@ import java.util.*
 
 interface MoreDetailsView
 
-internal class MoreDetailsViewActivity: MoreDetailsView, AppCompatActivity() {
+internal class MoreDetailsViewActivity : MoreDetailsView, AppCompatActivity() {
 
     private lateinit var moreDetailsPresenter: MoreDetailsPresenter
-    private lateinit var artistTextView: TextView
-    private lateinit var imageView: ImageView
-    private lateinit var openUrlButton: Button
+
+    private lateinit var lastFMArtistTextView: TextView
+    private lateinit var lastFMImageView: ImageView
+    private lateinit var lastFMOpenUrlButton: Button
+
+    private lateinit var wikipediaArtistTextView: TextView
+    private lateinit var wikipediaImageView: ImageView
+    private lateinit var wikipediaOpenUrlButton: Button
+
+    private lateinit var newYorkTimesArtistTextView: TextView
+    private lateinit var newYorkTimesImageView: ImageView
+    private lateinit var newYorkTimesOpenUrlButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,9 +49,17 @@ internal class MoreDetailsViewActivity: MoreDetailsView, AppCompatActivity() {
     }
 
     private fun initProperties() {
-        artistTextView = findViewById(R.id.artistInfoTextView)
-        imageView = findViewById(R.id.imageView)
-        openUrlButton = findViewById(R.id.openUrlButton)
+        lastFMImageView = findViewById(R.id.lastFMImageView)
+        lastFMArtistTextView = findViewById(R.id.lastFMArtistTextView)
+        lastFMOpenUrlButton = findViewById(R.id.lastFMOpenUrlButton)
+
+        wikipediaImageView = findViewById(R.id.wikipediaImageView)
+        wikipediaArtistTextView = findViewById(R.id.wikipediaArtistTextView)
+        wikipediaOpenUrlButton = findViewById(R.id.wikipediaOpenUrlButton)
+
+        newYorkTimesImageView = findViewById(R.id.newYorkTimesImageView)
+        newYorkTimesArtistTextView = findViewById(R.id.newYorkTimesArtistTextView)
+        newYorkTimesOpenUrlButton = findViewById(R.id.newYorkTimesOpenUrlButton)
     }
 
     private fun initObservers() {
@@ -49,44 +67,121 @@ internal class MoreDetailsViewActivity: MoreDetailsView, AppCompatActivity() {
             .subscribe { value -> updateArtistInfo(value) }
     }
 
-    private fun initMoreDetails(){
+    private fun initMoreDetails() {
         val artistName = intent.getStringExtra(ARTIST_NAME_EXTRA) ?: ""
         moreDetailsPresenter.moreDetails(artistName)
     }
 
-    private fun updateArtistInfo(artistUiState: MoreDetailsUiState){
-        updateView(artistUiState)
-        setURLButton(artistUiState)
-    }
-
-    private fun updateView(artistUiState: MoreDetailsUiState) {
-        runOnUiThread {
-            loadImageIntoView(artistUiState.logoUrl)
-            setArtistViewText(artistUiState.artistBioContent)
+    private fun updateArtistInfo(artistUiState: MoreDetailsUiState) {
+        when (artistUiState.source) {
+            "Wikipedia" -> updateUIWikipedia(artistUiState)
+            "LastFM" -> updateUILastFM(artistUiState)
+            "New York Times" -> updateUINYTimes(artistUiState)
         }
     }
 
-    private fun setURLButton(artistUiState: MoreDetailsUiState) {
-        val artistUrl = artistUiState.infoURL
-        setOpenUrlButton(artistUrl)
+    private fun updateUIWikipedia(artistUiState: MoreDetailsUiState) {
+        updateViewWikipedia(artistUiState)
+        setURLButtonWikipedia(artistUiState)
     }
 
-    private fun setOpenUrlButton(artistUrl: String) {
-        openUrlButton.setOnClickListener {
+    private fun updateUILastFM(artistUiState: MoreDetailsUiState) {
+        updateViewLastFM(artistUiState)
+        setURLButtonLastFM(artistUiState)
+    }
+
+    private fun updateUINYTimes(artistUiState: MoreDetailsUiState) {
+        updateViewNYTimes(artistUiState)
+        setURLButtonNYTimes(artistUiState)
+    }
+
+    private fun updateViewWikipedia(artistUiState: MoreDetailsUiState) {
+        runOnUiThread {
+            loadImageIntoViewWikipedia(artistUiState.logoUrl)
+            setArtistViewTextWikipedia(artistUiState.artistBioContent)
+        }
+    }
+
+    private fun setURLButtonWikipedia(artistUiState: MoreDetailsUiState) {
+        val artistUrl = artistUiState.infoURL
+        setOpenUrlButtonWikipedia(artistUrl)
+    }
+
+    private fun setOpenUrlButtonWikipedia(artistUrl: String) {
+        wikipediaOpenUrlButton.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(artistUrl)
             startActivity(intent)
         }
     }
 
-    private fun loadImageIntoView(logoUrl: String){
-        Picasso.get().load(logoUrl).into(imageView)
+    private fun loadImageIntoViewWikipedia(logoUrl: String) {
+        Picasso.get().load(logoUrl).into(wikipediaImageView)
     }
 
     @Suppress("DEPRECATION")
-    private fun setArtistViewText(artistInfoText: String){
-        artistTextView.text = Html.fromHtml(artistInfoText)
+    private fun setArtistViewTextWikipedia(artistInfoText: String) {
+        wikipediaArtistTextView.text = Html.fromHtml(artistInfoText)
     }
+
+    private fun updateViewLastFM(artistUiState: MoreDetailsUiState) {
+        runOnUiThread {
+            loadImageIntoViewLastFM(artistUiState.logoUrl)
+            setArtistViewTextLastFM(artistUiState.artistBioContent)
+        }
+    }
+
+    private fun setURLButtonLastFM(artistUiState: MoreDetailsUiState) {
+        val artistUrl = artistUiState.infoURL
+        setOpenUrlButtonLastFM(artistUrl)
+    }
+
+    private fun setOpenUrlButtonLastFM(artistUrl: String) {
+        lastFMOpenUrlButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(artistUrl)
+            startActivity(intent)
+        }
+    }
+
+    private fun loadImageIntoViewLastFM(logoUrl: String) {
+        Picasso.get().load(logoUrl).into(lastFMImageView)
+    }
+
+    @Suppress("DEPRECATION")
+    private fun setArtistViewTextLastFM(artistInfoText: String) {
+        lastFMArtistTextView.text = Html.fromHtml(artistInfoText)
+    }
+
+    private fun updateViewNYTimes(artistUiState: MoreDetailsUiState) {
+        runOnUiThread {
+            loadImageIntoViewNYTimes(artistUiState.logoUrl)
+            setArtistViewTextNYTimes(artistUiState.artistBioContent)
+        }
+    }
+
+    private fun setURLButtonNYTimes(artistUiState: MoreDetailsUiState) {
+        val artistUrl = artistUiState.infoURL
+        setOpenUrlButtonNYTimes(artistUrl)
+    }
+
+    private fun setOpenUrlButtonNYTimes(artistUrl: String) {
+        newYorkTimesOpenUrlButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(artistUrl)
+            startActivity(intent)
+        }
+    }
+
+    private fun loadImageIntoViewNYTimes(logoUrl: String) {
+        Picasso.get().load(logoUrl).into(newYorkTimesImageView)
+    }
+
+    @Suppress("DEPRECATION")
+    private fun setArtistViewTextNYTimes(artistInfoText: String) {
+        newYorkTimesArtistTextView.text = Html.fromHtml(artistInfoText)
+    }
+
 
     companion object {
         const val ARTIST_NAME_EXTRA = "artistName"
