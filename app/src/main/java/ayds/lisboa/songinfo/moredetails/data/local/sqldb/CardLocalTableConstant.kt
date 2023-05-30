ced59e4b0323b9765fab61b10e61c488187c13a6
Matias Pippig <matiaspippig@gmail.com>
2023-05-30 11:44:30 -0300

    Constante sin usar borrada

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/data/local/sqldb/CardLocalTableConstant.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/data/local/sqldb/CardLocalTableConstant.kt
@@ -1,6 +1,5 @@
 package ayds.lisboa.songinfo.moredetails.cards
 
-const val CARD_NAME = "card"
 const val ARTIST_NAME = "artistName"
 const val CARDS_TABLE = "cards"
 const val ID = "id"
@@ -16,7 +15,6 @@ const val createDataBaseTableQuery : String =
             "$ID integer PRIMARY KEY AUTOINCREMENT, " +
             "$ARTIST_NAME string," +
             "$SOURCE integer, " +
-            "$CARD_NAME string, " +
             "$DESCRIPTION string, " +
             "$INFO_URL string, " +
             "$SOURCE_LOGO_URL string)"
