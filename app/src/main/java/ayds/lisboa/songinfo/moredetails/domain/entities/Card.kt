e8d4b69a3f4ef569626f9416e95a7b7b5dfe4b63
Matias Pippig <matiaspippig@gmail.com>
2023-05-31 11:45:10 -0300

    Agregado enumerado para el source
    Co-authored-by: AxelFontana <axelfontana09@gmail.com>
    Co-authored-by: KatherinaVilca <katherinavilca@gmail.com>
    Co-authored-by: MartinVerniere <martincho7verniere@yahoo.com>

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/domain/entities/Card.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/domain/entities/Card.kt
@@ -2,11 +2,17 @@ package ayds.lisboa.songinfo.moredetails.domain.entities
 
 sealed class Card {
     data class CardData(
-        val source: String,
+        val source: Source,
         val description: String,
         val infoURL: String,
         var sourceLogoURL: String,
         var isLocallyStored: Boolean = false
     ) : Card()
     object EmptyCard : Card()
+}
+
+enum class Source {
+    CARD1,
+    CARD2,
+    CARD3
 }
