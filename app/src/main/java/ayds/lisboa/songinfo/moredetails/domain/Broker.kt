2d2b388a9da6d710631b1579da724b9a34c9daf7
Matias Pippig <matiaspippig@gmail.com>
2023-05-31 12:02:08 -0300

    creada la interfaz del Broker
    Co-authored-by: AxelFontana <axelfontana09@gmail.com>
    Co-authored-by: KatherinaVilca <katherinavilca@gmail.com>
    Co-authored-by: MartinVerniere <martincho7verniere@yahoo.com>

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/domain/Broker.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/domain/Broker.kt
@@ -1,22 +1,25 @@
 package ayds.lisboa.songinfo.moredetails.domain
 
-import ayds.lisboa.songinfo.moredetails.domain.proxy.ProxyNYTimes
-import ayds.lisboa.songinfo.moredetails.domain.proxy.ProxyLastFM
-import ayds.lisboa.songinfo.moredetails.domain.proxy.ProxyWikipedia
+import ayds.lisboa.songinfo.moredetails.domain.proxy.ProxyCard3
+import ayds.lisboa.songinfo.moredetails.domain.proxy.ProxyCard1
 import ayds.lisboa.songinfo.moredetails.domain.entities.Card
+import ayds.lisboa.songinfo.moredetails.domain.proxy.ProxyCard2
 
-class Broker(
-    private val proxyWiki: ProxyWikipedia,
-    private val proxyLastFM: ProxyLastFM,
-    private val proxyNYTimes: ProxyNYTimes
-) {
+interface Broker{
+    fun getCards(artistName: String): List<Card>
+}
+class BrokerImpl(
+    private val proxyCard1: ProxyCard1,
+    private val proxyCard2: ProxyCard2,
+    private val proxyCard3: ProxyCard3
+) : Broker{
 
-    fun getCards(artistName: String): List<Card> {
+    override fun getCards(artistName: String): List<Card> {
         val cardList = ArrayList<Card>()
 
-        cardList.add(proxyWiki.getCard(artistName))
-        cardList.add(proxyLastFM.getCard(artistName))
-        cardList.add(proxyNYTimes.getCard(artistName))
+        cardList.add(proxyCard1.getCard(artistName))
+        cardList.add(proxyCard2.getCard(artistName))
+        cardList.add(proxyCard3.getCard(artistName))
 
         return cardList.filterIsInstance<Card.CardData>()
     }
