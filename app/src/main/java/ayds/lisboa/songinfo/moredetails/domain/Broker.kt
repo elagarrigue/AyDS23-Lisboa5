de550e685a5e7fd811add0d0843ac845e6c60182
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-02 15:36:50 -0300

    Now proxys are of type ProxyCard

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/domain/Broker.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/domain/Broker.kt
@@ -1,18 +1,17 @@
 package ayds.lisboa.songinfo.moredetails.domain
 
-import ayds.lisboa.songinfo.moredetails.domain.proxy.ProxyCard3
-import ayds.lisboa.songinfo.moredetails.domain.proxy.ProxyCard1
 import ayds.lisboa.songinfo.moredetails.domain.entities.Card
-import ayds.lisboa.songinfo.moredetails.domain.proxy.ProxyCard2
+import ayds.lisboa.songinfo.moredetails.domain.proxy.ProxyCard
 
-interface Broker{
+interface Broker {
     fun getCards(artistName: String): List<Card>
 }
+
 class BrokerImpl(
-    private val proxyCard1: ProxyCard1,
-    private val proxyCard2: ProxyCard2,
-    private val proxyCard3: ProxyCard3
-) : Broker{
+    private val proxyCard1: ProxyCard,
+    private val proxyCard2: ProxyCard,
+    private val proxyCard3: ProxyCard
+) : Broker {
 
     override fun getCards(artistName: String): List<Card> {
         val cardList = ArrayList<Card>()
