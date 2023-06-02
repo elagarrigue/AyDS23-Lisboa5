b67756557b567aa420a6293a784fc1fe125da604
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-02 15:51:07 -0300

    Now getSource doesnt use factories, returns string directly

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/presentation/CardSourceHelper.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/presentation/CardSourceHelper.kt
@@ -2,40 +2,16 @@ package ayds.lisboa.songinfo.moredetails.presentation
 
 import ayds.lisboa.songinfo.moredetails.domain.entities.Source
 
-interface CardSourceHelper{
-    fun getSource(source: Source):SourceCreator
+interface CardSourceHelper {
+    fun getSource(source: Source): String
 }
 
-internal class CardSourceHelperImpl:CardSourceHelper {
-    override fun getSource(source: Source):SourceCreator {
-        return when (source){
-            Source.WIKIPEDIA -> Card1Factory()
-            Source.LASTFM -> Card2Factory()
-            Source.NYTIMES -> Card3Factory()
+internal class CardSourceHelperImpl : CardSourceHelper {
+    override fun getSource(source: Source): String {
+        return when (source) {
+            Source.WIKIPEDIA -> "Wikipedia"
+            Source.LASTFM -> "Last FM"
+            Source.NYTIMES -> "New York Times"
         }
     }
-
-}
-
-sealed class SourceCreator{
-    abstract fun createSource(): String
-}
-
-private class Card1Factory : SourceCreator() {
-    override fun createSource(): String {
-        return "Wikipedia"
-    }
-
-}
-private class Card2Factory : SourceCreator() {
-    override fun createSource(): String {
-        return "Last FM"
-    }
-
-}
-private class Card3Factory() : SourceCreator() {
-    override fun createSource(): String {
-        return "New York Times"
-    }
-
 }
