8934b101aa2871aaf7eeec08b2ca689d129fe6b7
Katherina <katherinavilca@gmail.com>
2023-06-05 13:47:15 -0300

    nombre mas significativo a getSource

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/presentation/CardSourceFactory.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/presentation/CardSourceFactory.kt
@@ -3,11 +3,11 @@ package ayds.lisboa.songinfo.moredetails.presentation
 import ayds.lisboa.songinfo.moredetails.domain.entities.Source
 
 interface CardSourceFactory {
-    fun getSource(source: Source): String
+    fun getSourceTitle(source: Source): String
 }
 
 internal class CardSourceFactoryImpl : CardSourceFactory {
-    override fun getSource(source: Source): String {
+    override fun getSourceTitle(source: Source): String {
         return when (source) {
             Source.WIKIPEDIA -> "Wikipedia"
             Source.LASTFM -> "Last FM"
