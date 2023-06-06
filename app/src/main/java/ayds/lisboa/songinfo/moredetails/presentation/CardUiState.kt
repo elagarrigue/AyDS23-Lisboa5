442da4a39f6b078082d7bffd5e28b4c3f17ea114
AxelFontana <axelfontana01@gmail.com>
2023-06-06 16:12:49 -0300

    deleted Source property

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/presentation/CardUiState.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/presentation/CardUiState.kt
@@ -1,10 +1,7 @@
 package ayds.lisboa.songinfo.moredetails.presentation
 
-import ayds.lisboa.songinfo.moredetails.domain.entities.Source
-
 
 data class CardUiState(
-    val source: Source = Source.EMPTY_SOURCE,
     val artistBioContent: String = "",
     val infoURL: String = "",
     val logoUrl: String = "",
--- app/src/main/java/ayds/lisboa/songinfo/moredetails/presentation/MoreDetailsPresenter.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/presentation/MoreDetailsPresenter.kt
@@ -2,12 +2,12 @@ package ayds.lisboa.songinfo.moredetails.presentation
 
 
 import ayds.lisboa.songinfo.moredetails.domain.entities.Card
-import ayds.lisboa.songinfo.moredetails.domain.entities.Source
 import ayds.lisboa.songinfo.moredetails.domain.repository.CardRepository
 import ayds.observer.Observable
 import ayds.observer.Subject
 
 private const val SOURCE = "Source: "
+
 interface MoreDetailsPresenter {
     val artistObservable: Observable<List<CardUiState>>
 
@@ -18,7 +18,7 @@ internal class MoreDetailsPresenterImpl(
     private val cardDescriptionHelper: CardDescriptionHelper,
     private val repository: CardRepository,
     private val cardSourceFactory: CardSourceFactory
-    ) :
+) :
     MoreDetailsPresenter {
 
     override val artistObservable = Subject<List<CardUiState>>()
@@ -29,26 +29,27 @@ internal class MoreDetailsPresenterImpl(
         }.start()
     }
 
-    private fun fetchMoreDetails(artistName: String){
+    private fun fetchMoreDetails(artistName: String) {
         val moreDetailsUiStates = getMoreDetailsUiState(artistName)
         artistObservable.notify(moreDetailsUiStates)
     }
+
     private fun getMoreDetailsUiState(artistName: String): List<CardUiState> {
         val cards = repository.getCards(artistName)
-        val cardUiStates : MutableList<CardUiState> = ArrayList()
+        val cardUiStates: MutableList<CardUiState> = ArrayList()
 
         if (cards.isEmpty()) {
             cardUiStates.add(updateCardNoResultsUiState())
         } else {
-            for (card in cards){
-                val reformattedText = cardDescriptionHelper.getCardDescription(card,artistName)
+            for (card in cards) {
+                val reformattedText = cardDescriptionHelper.getCardDescription(card, artistName)
                 cardUiStates.add(updateCardUiState(card, reformattedText))
             }
         }
         return cardUiStates
     }
 
-    private fun updateCardUiState(card: Card, reformattedText: String):CardUiState{
+    private fun updateCardUiState(card: Card, reformattedText: String): CardUiState {
         return when (card) {
             is Card.CardData -> updateUiState(card, reformattedText)
             Card.EmptyCard -> updateCardNoResultsUiState()
@@ -57,7 +58,6 @@ internal class MoreDetailsPresenterImpl(
 
     private fun updateCardNoResultsUiState(): CardUiState {
         return CardUiState(
-            Source.EMPTY_SOURCE,
             "No results",
             "",
             "",
@@ -65,20 +65,19 @@ internal class MoreDetailsPresenterImpl(
         )
     }
 
-    private fun getCardSource(card : Card.CardData): String {
+    private fun getCardSource(card: Card.CardData): String {
         return cardSourceFactory.getSourceTitle(card.source)
     }
-    private fun getSourceDescription(card: Card.CardData)
-       = "$SOURCE${getCardSource(card)}"
+
+    private fun getSourceDescription(card: Card.CardData) = "$SOURCE${getCardSource(card)}"
 
 
     private fun updateUiState(card: Card.CardData, reformattedText: String): CardUiState {
         return CardUiState(
-            card.source,
             reformattedText,
             card.infoURL,
             card.sourceLogoURL,
-           getSourceDescription(card)
+            getSourceDescription(card)
         )
     }
 
